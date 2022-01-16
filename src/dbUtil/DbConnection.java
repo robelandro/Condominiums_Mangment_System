0c8504a19d77e6a6852e01eddc2b9d2f47c64c85
robelandro <robelpopandro@gmail.com>
2022-01-16 15:11:51 -0300

    database added

--- src/dbUtil/DbConnection.java
+++ src/dbUtil/DbConnection.java
@@ -1,4 +1,30 @@
 package dbUtil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    private static final String DB_DRIVER ="org.sqlite.JDBC";
+    private static final String USERNAME ="dbUser";
+    private static final String PASSWORD ="dbPassword";
+    private static final String SQCONN ="jdbc:sqlite:cms.sqlite";
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        conn = DriverManager.getConnection(SQCONN);
+        if (conn != null){
+            System.out.println("Database Connection Successfully");
+        }
+        else {
+            System.out.println("Database Connection Failed");
+        }
+        return conn;
+    }
 }
