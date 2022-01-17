53bd506d60acd64b68d2003c9a13cd5e9380fae5
robelandro <robelpopandro@gmail.com>
2022-01-17 09:48:59 -0300

    update

--- src/helper/Resident.java
+++ src/helper/Resident.java
@@ -7,14 +7,32 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Resident extends Person{
-    public Resident(String firstName, String lastName, int age, String address, String sex) {
-        super(firstName, lastName, age, address, sex);
-    }
-    public void registration(){
+    private int blockNumber;
+    private int phoneNumber;
+    private int houseNumber;
 
+    public Resident(String firstName, String lastName, int age, String address, String sex, int blockNumber, int phoneNumber, int houseNumber) {
+        super(firstName, lastName, age, address, sex);
+        this.setBlockNumber(blockNumber);
+        this.setPhoneNumber(phoneNumber);
+        this.setHouseNumber(houseNumber);
+    }
+
+    public void registration(){
+        String tableName = "Resident";
+        createTable(tableName);
+        addData(tableName,getFirstName(),getLastName(),getAddress(),getAge(),getSex(),getPhoneNumber(),getBlockNumber(),getHouseNumber());
     }
     public static void createTable(String tableName){
-        String sqlTable = "CREATE TABLE "+tableName+" (Id INTEGER PRIMARY KEY AUTOINCREMENT,FirstName STRING,LastName STRING,Id_Number INTEGER,Age INTEGER,Gender STRING)";
+        String sqlTable = "CREATE TABLE "+tableName+" (Id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "FirstName STRING,"+
+                "LastName STRING,"+
+                "Address STRING,"+
+                "Age INTEGER,"+
+                "Sex STRING,"+
+                "PhoneNumber INTEGER,"+
+                "BlockNumber INTEGER,"+
+                "HouseNumber INTEGER)";
         Connection connection ;
         PreparedStatement statement ;
         try {
@@ -30,5 +48,51 @@ public class Resident extends Person{
 
             /*System.exit(1);*/
         }
+    }
+    public static void addData(String tableName,String firstName,String lastName,String address ,int age ,String sex,int phoneNumber,int blockNumber,int houseNumber){
+        String sqlInsert = "INSERT INTO "+tableName+"( FirstName,LastName,Address,Age,Sex,PhoneNumber,BlockNumber,HouseNumber) VALUES (?,?,?,?,?,?,?,?)";
+        Connection connection ;
+        PreparedStatement statement ;
+        try {
+            connection = DbConnection.getConnection();
+            statement =connection.prepareStatement(sqlInsert);
+            statement.setString(1,firstName);
+            statement.setString(2,lastName);
+            statement.setString(3,address);
+            statement.setInt(4,age);
+            statement.setString(5,sex);
+            statement.setInt(6,phoneNumber);
+            statement.setInt(7,blockNumber);
+            statement.setInt(8,houseNumber);
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getBlockNumber() {
+        return blockNumber;
+    }
+
+    public void setBlockNumber(int blockNumber) {
+        this.blockNumber = blockNumber;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
     }
 }
