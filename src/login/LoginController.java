2d39efac5ead659bfc8bb85af96363495f5cc24c
robelandro <robelpopandro@gmail.com>
2022-01-19 04:01:07 -0300

    update

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -1,6 +1,7 @@
 package login;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,6 +9,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +18,9 @@ public class LoginController implements Initializable {
 
     @FXML
     private Label dbStatues;
+
+    @FXML
+    private Label errorPrint;
 
     @FXML
     private Button forgetPassword;
@@ -35,5 +40,56 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.option.setItems(FXCollections.observableArrayList(Option.values()));
+    }
+
+    public LoginController(PasswordField password, TextField userName) {
+        this.setPassword(password);
+        this.setUserName(userName);
+    }
+
+    public void loginPressed(ActionEvent event){
+        LoginMode loginMode = new LoginMode();
+        String tableName ="Login";
+        String comboBox=((Option)this.option.getValue()).toString();
+        if (loginMode.isCorrect(tableName,getUserName().getText(),getPassword().getText(),comboBox)){
+            Stage stage = (Stage)this.login.getScene().getWindow();
+            switch (comboBox){
+                case "Admin":
+                    stage.close();
+                    adminDashBord();
+                    break;
+                case "Staff":
+                    stage.close();
+                    staffDashBord();
+                    break;
+                case default :
+                    errorPrint.setText("Option Is not Selected");
+            }
+        }
+        else {
+            errorPrint.setText("Wrong Credential");
+        }
+
+    }
+    public void adminDashBord(){
+
+    }
+    public void staffDashBord(){
+
+    }
+    public PasswordField getPassword() {
+        return password;
+    }
+
+    public void setPassword(PasswordField password) {
+        this.password = password;
+    }
+
+    public TextField getUserName() {
+        return userName;
+    }
+
+    public void setUserName(TextField userName) {
+        this.userName = userName;
     }
 }
--- src/login/LoginMode.java
+++ src/login/LoginMode.java
@@ -1,5 +1,38 @@
 package login;
 
+import dbUtil.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginMode {
+
+    public boolean isCorrect(String tableName,String userName,String password,String accessLeve){
+        Connection connection;
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        String sqlCheck="SELECT * FROM "+tableName+" WHERE UserId = 1 AND UserName =? AND Password =? AND AccessLevel =?";
+        try{
+            connection = DbConnection.getConnection();
+            preparedStatement = connection.prepareStatement(sqlCheck);
+            preparedStatement.setString(1,userName);
+            preparedStatement.setString(2,password);
+            preparedStatement.setString(3,accessLeve);
+            resultSet = preparedStatement.executeQuery();
+            preparedStatement.close();
+            connection.close();
+            resultSet.close();
+            if (resultSet.next()){
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 
 }
